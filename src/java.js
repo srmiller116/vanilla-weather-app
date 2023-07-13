edbc7d481e69bb39f6f641fe82cb7f68ffd588b0
srmiller116 <srmiller116@gmail.com>
2023-07-13 13:29:58 -0400

    Call Forecast Div in Java

--- src/java.js
+++ src/java.js
@@ -6,6 +6,7 @@ function citySearch(event) {
   let apiKey = "3f6be1c407b0d9d1933561808db358ba";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${inputCity.value}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showTemperature);
+  displayForecast();
 }
 
 function searchCurrentLocation() {
@@ -23,6 +24,7 @@ function showPosition(position) {
   displayCurrent.innerHTML = `Results for lat = ${myLatitude} long = ${myLongitude}`;
 
   axios.get(apiUrl).then(showTemperature);
+  displayForecast();
 }
 function showTemperature(response) {
   let temperatureRound = Math.round(response.data.main.temp);
@@ -100,3 +102,29 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", convertToCelsius);
 
 let celciusTemperature = null;
+
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thurs", "Fri", "Sat", "Sun"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      ` 
+            <div class="col-2">
+              <div class="weather-forecast-date">${day}</div>
+              <img
+                src="http://openweathermap.org/img/wn/04d@2x.png"
+                alt="icon"
+                width="36"
+              />
+              <div class="weather-forecast-temperatures">
+                <span class="weather-forecast-temperature-max">18°</span>
+                <span class="weather-forecast-temperature-min">12°</span>
+              </div>
+            </div>
+         `;
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
